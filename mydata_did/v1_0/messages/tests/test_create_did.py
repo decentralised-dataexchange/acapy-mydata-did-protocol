2d3b6c464accf9a132192e5ac9723005afffbd89
George J Padayatti <george.padayatti@igrant.io>
2022-02-16 00:24:39 +0530

    Deleted dummy tests
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- mydata_did/v1_0/messages/tests/test_create_did.py
+++
@@ -1,9 +0,0 @@
-from unittest import mock, TestCase
-
-class TestCreateDID(TestCase):
-    def setUp(self) -> None:
-        return super().setUp()
-    
-    def test_init(self):
-        """Test initialization"""
-        assert False
